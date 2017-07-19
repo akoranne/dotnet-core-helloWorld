c7468c740fd0eb9b2719947539cff71071e2eb63
Ajay Koranne <akoranne@gmail.com>
2017-07-19 14:12:50 -0700

    added build-output folder

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,8 +2,13 @@
 cd service-repo
 
 echo I am in `pwd`
+ls -l ../
+
+echo "starting build ..."
 
 dotnet restore
 dotnet publish -c Release -o ./publish
 
-cp manifest.yml ./publish/
+echo "copying files to ../build-output"
+cp manifest.yml ../build-output
+cp ./publish/* ../build-output
